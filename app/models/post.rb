f2285c69369cad37981e3dd37b89632f56996c38
alexMATE <sweetflavordisign@gmail.com>
2015-12-06 22:21:00 +0000

    Som style

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"},
+                    default_url: ActionController::Base.helpers.asset_path('barco2.png')
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
